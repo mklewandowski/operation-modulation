8996293641fe360e41366a9302de3b3f4ea7966b
mklewandowski <matt.kain.lewandowski@gmail.com>
2023-06-26 11:41:26 -0700

    Add class to hold binary images

---
+++ Assets/Scripts/BinaryImage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryImage : MonoBehaviour
+{
+    public int[] Bits;
+    public string Title;
+    public string Description;
+}
