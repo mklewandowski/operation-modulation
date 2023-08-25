ae9633309fdf0b4ef1cc80e25975565c60318f50
mklewandowski <matt.kain.lewandowski@gmail.com>
2023-08-25 16:56:38 -0500

    Add translation text

--- Assets/Scripts/GameSceneManagerDDR.cs
+++ Assets/Scripts/GameSceneManagerDDR.cs
@@ -152,22 +152,28 @@ public class GameSceneManagerDDR : MonoBehaviour
         {
             binaryImages[x] = new BinaryImage();
         }
-        binaryImages[0].Title = "smiley emoji";
+        binaryImages[0].Title = "smile emoji";
+        binaryImages[0].TitleSP = "smile emoji";
         binaryImages[0].isASK = true;
         binaryImages[0].Bits = new int[64] {0,0,1,1,1,1,0,0,0,1,0,1,1,0,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,0,0,0,0,1,1,0,1,1,1,1,1,1,0,0,0,1,1,1,1,0,0};
         binaryImages[1].Title = "heart";
+        binaryImages[1].TitleSP = "heart";
         binaryImages[1].isASK = false;
         binaryImages[1].Bits = new int[64] {0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0,1,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,0,1,1,0,0,0};
         binaryImages[2].Title = "cat";
+        binaryImages[2].TitleSP = "cat";
         binaryImages[2].isASK = true;
         binaryImages[2].Bits = new int[64] {1,0,0,1,0,0,0,1,1,1,1,1,0,0,1,0,1,1,1,1,0,0,1,0,0,1,1,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,0,1,0};
         binaryImages[3].Title = "robot";
+        binaryImages[3].TitleSP = "robot";
         binaryImages[3].isASK = false;
         binaryImages[3].Bits = new int[64] {0,1,0,0,0,0,1,0,0,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,0,1,0,1,1,0,1,0,1,1,1,1,1,1,1,1,0,1,1,0,0,1,1,0,0,0,1,0,0,1,0,0,0,0,1,1,1,1,0,0};
-        binaryImages[4].Title = "true ship";
+        binaryImages[4].Title = "space ship";
+        binaryImages[4].TitleSP = "space ship";
         binaryImages[4].isASK = false;
         binaryImages[4].Bits = new int[64] {0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,1,1,1,0,0,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1};
         binaryImages[5].Title = "ghost";
+        binaryImages[5].TitleSP = "ghost";
         binaryImages[5].isASK = false;
         binaryImages[5].Bits = new int[64] {0,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,1,0,0,1,0,0,1,0,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1,0,1,0,1,0};
     }
@@ -200,12 +206,39 @@ public class GameSceneManagerDDR : MonoBehaviour
     void UpdateIntroPanel()
     {
         HUDLevelStartImage.sprite = binaryImages[currentLevel].isASK ? ASKintroSprite : FSKintroSprite;
-        string introText = "A " + binaryImages[currentLevel].Title + " image is being sent using radio waves and you must decode it!\n\n";
-        introText = introText + "First we convert the squares to binary numbers. White squares are represented as a 1. Black squares are represented as a 0.\n\n";
-        introText = introText + "Then we turn the binary numbers into a radio signal that uses " + (binaryImages[currentLevel].isASK ? "amplitude shift keying. " : "frequency shift keying. ");
-        introText = introText + (binaryImages[currentLevel].isASK ? "0's are represented with a low amplitude signal, 1's are represented with a high amplitude signal." : "0's are represented with a low frequency signal, 1's are represented with a high frequency signal.");
+        string introText = "A radio wave carrying the digital information of a " + binaryImages[currentLevel].Title + " is coming your way!\n\n";
+        introText = "";
+        if (Globals.CurrentLanguage == Globals.Language.English)
+        {
+            introText = "A radio wave carrying the digital information of a " + binaryImages[currentLevel].Title + " is coming your way!\n\n";
+            introText = introText + "Now YOU are the computer and must decode the digitally modulated carrier signal correctly!\n\n";
+            if (binaryImages[currentLevel].isASK)
+            {
+                introText = introText + "As the radio wave using amplitude key shifting comes in, decide if the waveform shows a 0 or a 1 based on its amplitude. White squares are represented as a 1. Black squares are represented as a 0. At the end we will compare your decoded image to the one sent!";
+            }
+            else
+            {
+                introText = introText + "As the radio wave using frequency key shifting comes in, decide if the waveform shows a 0 or a 1 based on its frequency. White squares are represented as a 1. Black squares are represented as a 0. At the end we will compare your decoded image to the one sent!";
+            } 
+        }
+        else
+        {
+            introText = "A radio wave carrying the digital information of a " + binaryImages[currentLevel].TitleSP + " is coming your way!\n\n";
+            introText = introText + "Now YOU are the computer and must decode the digitally modulated carrier signal correctly!\n\n";
+            if (binaryImages[currentLevel].isASK)
+            {
+                introText = introText + "As the radio wave using amplitude key shifting comes in, decide if the waveform shows a 0 or a 1 based on its amplitude. White squares are represented as a 1. Black squares are represented as a 0. At the end we will compare your decoded image to the one sent!";
+            }
+            else
+            {
+                introText = introText + "As the radio wave using frequency key shifting comes in, decide if the waveform shows a 0 or a 1 based on its frequency. White squares are represented as a 1. Black squares are represented as a 0. At the end we will compare your decoded image to the one sent!";
+            } 
+        }
         HUDLevelStartText.text = introText;
-        HUDLevelStartTitleText.text = "LEVEL " + (currentLevel + 1).ToString();
+        if (Globals.CurrentLanguage == Globals.Language.English)
+            HUDLevelStartTitleText.text = "LEVEL " + (currentLevel + 1).ToString();
+        else
+            HUDLevelStartTitleText.text = "LEVEL " + (currentLevel + 1).ToString();
 
         for (int x = 0; x < MaxSquares; x++)
         {
@@ -266,8 +299,16 @@ public class GameSceneManagerDDR : MonoBehaviour
                 BinaryImageSquaresLevelCompleteUser[x].GetComponent<BinaryImageSquare>().SquareImage.color = Color.black;
             }
         }
-        HUDLevelCompletePercent.text = ((int)((float)numCorrect / 64f * 100f)).ToString() + "% accurate!";
-        HUDLevelButtonText.text = (currentLevel < 4 ? "NEXT LEVEL" : "HOME");
+        if (Globals.CurrentLanguage == Globals.Language.English)
+        {
+            HUDLevelCompletePercent.text = ((int)((float)numCorrect / 64f * 100f)).ToString() + "% accurate!";
+            HUDLevelButtonText.text = (currentLevel < 4 ? "NEXT LEVEL" : "HOME");
+        }
+        else
+        {
+            HUDLevelCompletePercent.text = ((int)((float)numCorrect / 64f * 100f)).ToString() + "% accurate!";
+            HUDLevelButtonText.text = (currentLevel < 4 ? "NEXT LEVEL" : "HOME"); 
+        }
     }
 
     void CreateScrollingGraph()
